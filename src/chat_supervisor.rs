55fa866ee566f5bc65ec86ed4eb73d5d9ca7d982
tsorak <karost0249@gmail.com>
2024-02-20 09:49:03 +0100

    refactor commandswitch

--- src/chat_supervisor.rs
+++ src/chat_supervisor.rs
@@ -1,8 +1,11 @@
 use crate::chat;
 use tokio::{
-    sync::mpsc::Receiver,
+    sync::mpsc::{channel, Receiver, Sender},
     task::{AbortHandle, JoinHandle},
 };
+
+#[derive(Debug)]
+pub struct Channel(pub Sender<Message>, pub Receiver<Message>);
 
 #[derive(Debug)]
 pub enum WsCommand {
@@ -13,35 +16,53 @@ pub enum WsCommand {
 #[derive(Debug)]
 pub struct Message(WsCommand, String);
 
-pub fn init(mut rx: Receiver<Message>) -> JoinHandle<()> {
-    tokio::spawn(async move {
-        let mut chat_handle: Option<AbortHandle> = None;
+impl Channel {
+    pub fn new(bfr: usize) -> Self {
+        let (tx, rx) = channel(bfr);
+        Self(tx, rx)
+    }
 
-        loop {
-            match rx.recv().await.unwrap() {
-                Message(WsCommand::Join, channel_name) => {
-                    if let Some(chat_handle) = &chat_handle {
-                        chat_handle.abort();
+    pub fn init(mut rx: Receiver<Message>) -> JoinHandle<()> {
+        tokio::spawn(async move {
+            let mut chat_handle: Option<AbortHandle> = None;
+
+            loop {
+                match rx.recv().await {
+                    None => (),
+                    Some(Message(WsCommand::Join, channel_name)) => {
+                        if let Some(chat_handle) = &chat_handle {
+                            chat_handle.abort();
+                        }
+
+                        chat_handle = Some(
+                            tokio::spawn(async move {
+                                chat::init(&channel_name).await;
+                            })
+                            .abort_handle(),
+                        );
                     }
-
-                    chat_handle = Some(
-                        tokio::spawn(async move {
-                            chat::init(&channel_name).await;
-                        })
-                        .abort_handle(),
-                    );
-                }
-                Message(WsCommand::Leave, _) => {
-                    if let Some(chat_handle) = &chat_handle {
-                        chat_handle.abort();
-                        println!("PoroSad ANYWAYS...")
-                    } else {
-                        println!("You aren't even in a channel LuL!")
+                    Some(Message(WsCommand::Leave, _)) => {
+                        if let Some(chat_handle) = &chat_handle {
+                            chat_handle.abort();
+                            println!("PoroSad ANYWAYS...")
+                        } else {
+                            println!("You aren't even in a channel LuL!")
+                        }
                     }
                 }
             }
+        })
+    }
+
+    pub fn send(tx: &Sender<Message>, (cmd, arg1): (&str, &str)) {
+        let tx = tx.clone();
+
+        if let Some(Message(cmd, arg1)) = (cmd, arg1).try_into().ok() {
+            tokio::spawn(async move {
+                let _ = tx.send(Message(cmd, arg1)).await;
+            });
         }
-    })
+    }
 }
 
 impl TryFrom<(&str, &str)> for Message {
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,16 @@
-use tokio::sync::mpsc::{channel, Sender};
-
 mod chat;
 mod chat_supervisor;
 mod input;
 
+use chat_supervisor as cs;
+
 #[tokio::main]
 async fn main() {
     let mut input = input::Channel::new(10);
-    let (supervisor_tx, supervisor_rx) = channel::<chat_supervisor::Message>(10);
+    let cs::Channel(sup_tx, sup_rx) = cs::Channel::new(10);
 
     input.init_stdin_read_loop();
-    chat_supervisor::init(supervisor_rx);
-
-    let mut is_first_loop = true;
+    cs::Channel::init(sup_rx);
 
     println!("Entering command read loop");
     loop {
@@ -26,32 +24,15 @@ async fn main() {
 
             match (cmd, arg1) {
                 (_cmd, "") => continue,
-                ("join", ch) | ("j", ch) => {
-                    let _ = &supervisor_tx.send(("join", ch).try_into().unwrap()).await;
-                }
+                ("join", ch) | ("j", ch) => cs::Channel::send(&sup_tx, ("join", ch)),
                 _ => continue,
             }
         } else {
             match stdinput.as_str() {
-                "q" => {
-                    println!("Bye bye");
-                }
-                "leave" | "ds" | "d" => {
-                    let _ = sup_send(&supervisor_tx, ("leave", "")).await;
-                }
-                _ if is_first_loop => {
-                    sup_send(&supervisor_tx, ("join", "")).await;
-                }
+                "q" => println!("Bye bye"),
+                "leave" | "ds" | "d" => cs::Channel::send(&sup_tx, ("leave", "")),
                 _ => continue,
             }
         }
-
-        if is_first_loop {
-            is_first_loop = false;
-        }
     }
-}
-
-async fn sup_send(tx: &Sender<chat_supervisor::Message>, msg: (&str, &str)) {
-    let _ = tx.send(msg.try_into().unwrap()).await;
 }
